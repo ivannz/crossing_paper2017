cc1b8f94f630d489d8fc01d2fdbcf8df5130980e
Ivan Nazarov <ivannnnz@gmail.com>
2017-03-20 00:11:06 +0300

    Cython-related update of setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,20 @@ from pip.download import PipSession
 
 import numpy
 
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
+install_reqs = parse_requirements("requirements.txt",
+                                  session=PipSession())
 
 # python setup.py build_ext [--inplace]
 setup(
     name="crossing_tree",
-    ext_modules=cythonize([Extension("crossing_tree/_crossing", 
+    ext_modules=cythonize([Extension("crossing_tree._crossing",
                                      ["crossing_tree/_crossing.pyx"],
-                                     include_dirs=[numpy.get_include()],),]),
+                                     include_dirs=[numpy.get_include()],)]),
     cmdclass={"build_ext": build_ext},
-    packages=["crossing_tree","crossing_tree/processes",],
+    packages=["crossing_tree", "crossing_tree/processes"],
     author='Ivan Nazarov',
     version='0.9.2',
-    description="A library for experimentation with crossing trees for analysis of self-similarity",
+    description="""A library for experimentation with crossing trees """
+                """for analysis of self-similarity.""",
     install_requires=[str(ir.req) for ir in install_reqs],
 )
