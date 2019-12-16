240e20e35839d0261b725c8e8d5e5fcbf0155457
ivannz <ivannz@yandex.ru>
2019-12-16 12:57:00 +0300

    dfs slice update, python3.7 compat

--- crossing_tree/manager.py
+++ crossing_tree/manager.py
@@ -4,46 +4,59 @@
 import os
 import re
 import gzip
-import cPickle
+import pickle
 
-def depth_first_filter(cache, key):
+
+def depth_first_filter(d, key, *tail, prefix=(), memo=None):
     """Depth-first filtered traversal of a hierarchical
     dictionary. Requires that the key be padded.
     """
-    # Recursion depth is controlled by the length of the key.
+    if memo is None:
+        memo = set()
+
+    # self-reference protection
+    if id(d) in memo:
+        return
+
+    memo.add(id(d))
+
+    # Recursion depth is controlled by the length of the `tail`.
     #  pad the key with entire slices to limit the depth.
-    if not isinstance(key, (tuple, list)):
-        key = tuple([key])
+    if isinstance(key, slice):
+        if key.stop or key.start or key.step:
+            raise TypeError("""Only entire slices are supported.""")
+
+        keys = d.keys()
+
+    elif key in d:
+        keys = [key]
+
+    else:
+        return
 
     # Use entire slice, if the key has been exhausted.
-    key_ = key[0] if key else slice(None)
-    if isinstance(key_, slice):
-        if key_.stop or key_.start or key_.step:
-            raise TypeError("""Only entire slices are supported.""")
-        generator_ = cache.iteritems()
-    else:
-        if key_ not in cache:
-            raise StopIteration
-        generator_ = iter([(key_, cache[key_])])
+    if not tail:
+        tail = slice(None),
 
     # Depth-first traversal
-    for key_, value_ in generator_:
-        if len(key) > 1:
-            for k, v in depth_first_filter(value_, key[1:]):
-                yield ((key_,) + k, v)
+    for key in keys:
+        if isinstance(d[key], dict):
+            yield from depth_first_filter(d[key], *tail, prefix=(*prefix, key))
+
         else:
-            yield ((key_,), value_)
+            yield (*prefix, key), d[key]
+
 
 class Hierarchy(object):
     """A simple container for hierarchy."""
     def __init__(self, depth=5):
         self.depth = depth
         self.base = dict()
-    
+
     def __call__(self, key=None):
         if not isinstance(key, (tuple, list)):
             if key is None:
-                key = (Ellipsis,)
+                key = Ellipsis,
             else:
                 key = tuple([key])
 
@@ -58,13 +71,13 @@ class Hierarchy(object):
         else:
             left_, right_ = key, tuple()
 
-        pad_ = self.depth - len(left_) - len(right_)
-        if pad_ < 0:
+        n_pad = self.depth - len(left_) - len(right_)
+        if n_pad < 0:
             raise KeyError("""Invalid key (`%s`)"""%("`, `".join(
                 str(member_) for member_ in key),))
 
-        key_ = left_ + tuple([slice(None)]*pad_) + right_
-        return key_, depth_first_filter(self.base, key_)
+        key = left_ + tuple([slice(None)] * n_pad) + right_
+        return key, depth_first_filter(self.base, *key)
 
     def __getitem__(self, key):
         key_, iterator_ = self.__call__(key)
@@ -104,6 +117,7 @@ class Hierarchy(object):
             base_ = base_[key_]
         return True
 
+
 class ExperimentManager(object):
     """docstring for ExperimentManager"""
     def __init__(self, name_format):
@@ -113,7 +127,7 @@ class ExperimentManager(object):
 
         # Initialize the keys and cache
         self.keys_ = [tup_[0] for tup_ in
-                      sorted(self.format.groupindex.iteritems(),
+                      sorted(self.format.groupindex.items(),
                              key=lambda tup_: tup_[1])]
         self.experiments_ = Hierarchy(depth=len(self.keys_))
         self.cache_ = dict()
@@ -126,6 +140,7 @@ class ExperimentManager(object):
             key_ = self.check(filename_)
             if not key_:
                 continue
+
             self.experiments_[key_] = filename_
 
     def check(self, filename):
@@ -133,17 +148,20 @@ class ExperimentManager(object):
         matches_ = self.format.match(os.path.basename(filename))
         if matches_ is None:
             return None
+
         return tuple(matches_.group(key_) for key_ in self.keys_)
-    
+
     def load(self, filename):
         """Loads the results of an experiment.
         """
         info = self.check(filename)
         if info is None:
-            raise TypeError("""Cannot identify file: `%s`."""
-                            %(os.path.basename(filename),))
+            basename = os.path.basename(filename)
+            raise TypeError(f"Cannot identify file: `{basename}`.")
+
         with gzip.open(filename, "rb") as fin:
-            start, finish, seeds, results = cPickle.load(fin)
+            start, finish, seeds, results = pickle.load(fin, encoding="bytes")
+
         return info, start, finish, seeds, results
 
     def __contains__(self, key):
@@ -151,17 +169,17 @@ class ExperimentManager(object):
 
     def __getitem__(self, key):
         full_key_, iterator_ = self.experiments_(key)
-        
+
         list_ = list()
         for key_, filename_ in iterator_:
             if filename_ not in self.cache_:
                 self.cache_[filename_] = self.load(filename_)
             list_.append(self.cache_[filename_])
-    
+
         # Return a list of items if the key has a slice
         if any(isinstance(member_, slice) for member_ in full_key_):
             return list_
-        
+
         if len(list_) > 0:
             return list_[0]
 
